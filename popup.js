e43a25de8e028570b692f09433bc476cb9ea292d
David Li <david.li@duke.edu>
2020-12-02 13:03:57 -0500

    Convert to chrome extension

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var Button = document.getElementById('button');
+	Button.addEventListener('click', onClick();
+});
+
+function onClick() {
+	window.close();
+	window.open("inputs.html", "_blank", "height=600,width=850,top=-100");
+}
